a38a4b8bd133bdb9f8cebb709ec7b9661122620f
nelmerrosa <83226782+nelmerrosa@users.noreply.github.com>
2021-12-20 18:11:43 -0300

    Add files via upload

---
+++ Urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '43333',
+                nome: 'Carlinho',
+                partido: 'ABC',
+                fotos:[
+                    {url:'AVATARM.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '21111',
+                nome: 'Maria',
+                partido: 'BBB',
+                fotos:[
+                    {url:'AVATARF.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'André',
+                partido: 'ABC',
+                vice: 'Francisca',
+                fotos:[
+                    {url:'AVATARM.jpg', legenda: 'Prefeito'},
+                    {url:'AVATARF.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '55',
+                nome: 'Fernanda',
+                partido: 'BBB',
+                vice: 'João',
+                fotos:[
+                    {url:'AVATARF.jpg', legenda: 'Prefeito'},
+                    {url:'AVATARM.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
